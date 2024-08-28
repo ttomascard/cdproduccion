3cf393e07ebdff2a6ecbb1181c8adadfea52b271
tomascard <tomas.cardona@upb.edu.co>
2024-08-27 19:10:10 -0500

    Cambios en main.tf

--- iac-docker/main.tf
+++ iac-docker/main.tf
@@ -1,10 +1,27 @@
 terraform {
     required_providers {
-      docker = {
-        source = "kreuzwerker/docker"
-        version = "3.0.2"
-      }
+        docker = {
+            source  = "kreuzwerker/docker"
+            version = "3.0.2"
+        }
     }
 }
 
 provider "docker" {}
+
+resource "docker_container" "contenedorServWeb" {
+    image = docker_image.flaskimage.image_id
+    name = "laboratorio"
+    ports {
+        internal = 8080
+        external = 8081
+    }
+}
+
+resource "docker_image" "flaskimage" {
+    name = "laboratorio-flask"
+    build {
+        context="../flask-docker"
+        tag = ["laboratorio-ds"]
+    }
+}
