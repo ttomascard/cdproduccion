3a0dcc8a143393ab7231b2fbddb6c93d48adaeb1
tomascard <tomas.cardona@upb.edu.co>
2024-11-06 11:56:24 -0500

    borrado de recursos

--- iac-docker/main.tf
+++ iac-docker/main.tf
@@ -7,18 +7,18 @@ provider "azurerm" {
   }
 }
 
-resource "azurerm_storage_account" "laboratorio" {
-  name                     = "tomupbdsplaboratoriodev"
-  resource_group_name      = "dsproduccion202402"
-  location                 = "eastus"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  tags = {
-    environment = "dev"
-    owner = "tomas.cardona@upb.edu.co"
-    project = "tomas-ds-produccion"
-  }
-}
+# resource "azurerm_storage_account" "laboratorio" {
+#   name                     = "tomupbdsplaboratoriodev"
+#   resource_group_name      = "dsproduccion202402"
+#   location                 = "eastus"
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+#   tags = {
+#     environment = "dev"
+#     owner = "tomas.cardona@upb.edu.co"
+#     project = "tomas-ds-produccion"
+#   }
+# }
 
 # resource "docker_container" "contenedorServWeb" {
 #   image = docker_image.flaskimage.image_id
