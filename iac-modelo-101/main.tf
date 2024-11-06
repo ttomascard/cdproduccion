07edcac6b4a68d26360fe1076ad8ccdb90955a62
tomascard <tomas.cardona@upb.edu.co>
2024-11-06 11:15:33 -0500

    modelo101

---
+++ iac-modelo-101/main.tf
@@ -0,0 +1,27 @@
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+locals {
+  common_tags = {
+    environment = var.environment
+    project = "modelo-101"
+    owner  = "tomas.cardona@upb.edu.co"
+  }
+  prefix = "tom"
+  resource_group_name = "dsproduccion202402"
+}
+
+resource "azurerm_storage_account" "function_storage_account" {
+  name                     = "${local.prefix}${var.environment}upbfunc"
+  resource_group_name      = local.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  tags = local.common_tags
+}
---
+++ iac-modelo-101/variables.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  description = "The location of the resource group"
+  type        = string
+  
+}
+variable "environment" {
+  description = "The environment of the resource group"
+  type        = string
+}
