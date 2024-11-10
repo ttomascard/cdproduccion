760c5b7b3cdc968b2cc9a996ab1765efe46388f6
Tomas Cardona <tomas.cardona@upb.edu.co>
2024-11-10 20:00:31 +0000

    azure-function

--- iris/irismlops/backend-fn/function_app.py
+++ iris/irismlops/backend-fn/function_app.py
@@ -1,28 +1,73 @@
 import azure.functions as func
-import datetime
 import json
 import logging
+from keras.models import model_from_json
+import streamlit as st
+import numpy as np
+import requests
+
 
 app = func.FunctionApp()
 
+model = None
 
-@app.route(route="HttpExample", auth_level=func.AuthLevel.ANONYMOUS)
-def HttpExample(req: func.HttpRequest) -> func.HttpResponse:
+# Cargar el modelo
+def load_trained_model():
+    with open('price-house-v1.json') as json_file:
+        load_model_json = json_file.read()
+    model = model_from_json(load_model_json)
+    model.compile(optimizer='rmsprop', metrics=['mae'])
+    return model
+
+# Cargar el modelo al inicio
+model = load_trained_model()
+
+@app.route(route="predict", auth_level=func.AuthLevel.ANONYMOUS)
+def predict(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(json.dumps({"response":f"Hello, {name}. This HTTP triggered function executed successfully."}), mimetype="application/json", status_code=200)
-    else:
+    # Obtener los parámetros de la solicitud
+    try:
+        crime_rate = float(req.params.get('crime_rate'))
+        residential_land_zone = float(req.params.get('residential_land_zone'))
+        non_retail_business_acres = float(req.params.get('non_retail_business_acres'))
+        charles_river = float(req.params.get('charles_river'))
+        nitric_oxide = float(req.params.get('nitric_oxide'))
+        rooms_per_dwelling = float(req.params.get('rooms_per_dwelling'))
+        proportion_of_business_acres = float(req.params.get('proportion_of_business_acres'))
+        weight_of_the_house = float(req.params.get('weight_of_the_house'))
+        access_to_highways = float(req.params.get('access_to_highways'))
+        full_value = float(req.params.get('full_value'))
+        pupil_teacher_ratio = float(req.params.get('pupil_teacher_ratio'))
+        proportion_of_black_people = float(req.params.get('proportion_of_black_people'))
+        lower_status_population = float(req.params.get('lower_status_population'))
+    except ValueError:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+            "Invalid input data. Please check the values provided.",
+            status_code=400
         )
+
+    # Crear el array de entrada para la predicción
+    input_data = np.array([crime_rate, residential_land_zone, non_retail_business_acres, 
+                           charles_river, nitric_oxide, rooms_per_dwelling, 
+                           proportion_of_business_acres, weight_of_the_house, 
+                           access_to_highways, full_value, pupil_teacher_ratio, 
+                           proportion_of_black_people, lower_status_population]).reshape(1, 13)
+
+    # Obtener la predicción del modelo
+    price = model.predict(input_data)
+
+    # Devolver la respuesta en formato JSON
+    return func.HttpResponse(
+        json.dumps({"predicted_price": str(price[0][0])}),
+        mimetype="application/json",
+        status_code=200
+    )
+
+
+    
+    
+
+    
+
+   
--- iris/irismlops/iac/terraform/outputs.tf
+++ iris/irismlops/iac/terraform/outputs.tf
@@ -8,6 +8,11 @@ output "be_fn_container_app_fqdn" {
   description = "The FQDN of the backend function container app"
 }
 
+output "fe_fn_container_app_fqdn" {
+  value       = azurerm_container_app.frontend_function_container_app.latest_revision_fqdn
+  description = "The FQDN of the backend function container app"
+}
+
 
 output "fe_container_app_fqdn" {
   value       = azurerm_container_app.frontend_container_app.latest_revision_fqdn
